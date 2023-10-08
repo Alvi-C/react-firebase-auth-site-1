2a50d0bbf13785b007120fe38af0eacbe83cb519
Alvi Chowdhury <alvic5050@gmail.com>
2023-10-08 11:25:56 +0600

    google font & custom color added

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,25 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppins: "'Poppins', sans-serif",
+        italianno: "'Italianno', cursive",
+        catamaran: "'Catamaran', sans-serif",
+        roboto: "'Roboto Flex', sans-serif"
+      },
+      colors: {
+        'light-mistyRose': '#F9E6E6',
+        'light-oldLace': '#F9F6E6',
+        'light-alabaster': '#EBF9E6',
+        'light-honeydew': '#E6F9F1',
+        'light-lavender': '#E6F1F9',
+        'light-purpleDew': '#EBE6F9',
+        'light-pinkLace': '#FFDDF4',
+        'light-softGrey': '#f2f2f2',
+        'charcoal': '#36454F',
+      }
+    },
   },
   plugins: [],
 }
