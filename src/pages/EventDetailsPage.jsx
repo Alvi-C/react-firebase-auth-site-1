0ced4fdbb3c0c4e0b1d4b01518a314417387dbe8
Alvi Chowdhury <alvic5050@gmail.com>
2023-10-08 11:46:43 +0600

    few more pages added

---
+++ src/pages/EventDetailsPage.jsx
@@ -0,0 +1,11 @@
+
+
+const EventDetailsPage = () => {
+    return (
+        <div>
+            <h1>This is the event details page</h1>
+        </div>
+    );
+};
+
+export default EventDetailsPage;
---
+++ src/pages/LogInPage.jsx
@@ -0,0 +1,11 @@
+
+
+const LogInPage = () => {
+    return (
+        <div>
+            <h1>This is the log in page</h1>
+        </div>
+    );
+};
+
+export default LogInPage;
